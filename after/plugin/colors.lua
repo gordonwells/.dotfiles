42788d6f900a6cd14b0b161601a72195100e22c5
Gordon Wells <gcwells92@gmail.com>
2023-01-07 22:52:04 +0000

    Change theme to gruvbox

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,19 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-end
+require("gruvbox").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = true,
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "soft", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
 
-ColorMyPencils()
+vim.cmd.colorscheme("gruvbox")
--- lua/spiffoy/packer.lua
+++ lua/spiffoy/packer.lua
@@ -14,11 +14,8 @@ return require('packer').startup(function(use)
     }
 
     use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
+        'ellisonleao/gruvbox.nvim',
+        as = 'gruvbox',
     })
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
