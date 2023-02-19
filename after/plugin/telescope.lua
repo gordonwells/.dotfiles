df0c77bbba07fa7f5cfa8564c9ba48bcf2d6a223
Gordon Wells <gcwells92@gmail.com>
2023-02-19 18:24:49 +0000

    Make telescope search hidden files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,20 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>sf', function ()
+    builtin.find_files{
+        hidden = true,
+    }
+end)
+vim.keymap.set('n', '<leader>sg', function ()
+    builtin.live_grep{
+        hidden = true,
+    }
+end)
+
+require('telescope').setup{
+    defaults = {
+        options = {
+            search_hidden = true
+        },
+        file_ignore_patterns = {".git"}
+    }
+}
