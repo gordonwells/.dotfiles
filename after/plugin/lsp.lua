61aab5576070717625686ef646a926d8b9ec0662
Gordon Wells <gcwells92@gmail.com>
2023-02-19 18:24:31 +0000

    Reconfigure lsp shortcuts

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-	'sumneko_lua',
+	'lua_ls',
 	'pyright',
 })
 
@@ -11,8 +11,7 @@ local cmp_select = {behaviour = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	["C-Space>"] = cmp.mapping.complete(),
+	['<Tab>'] = cmp.mapping.confirm({ select = true }),
 })
 
 lsp.set_preferences({
@@ -26,10 +25,9 @@ lsp.setup_nvim_cmp({
 lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("i", "<C-Space>", function() vim.lsp.buf.signature_help() end, opts)
-
+	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set('i', '<C-Space>', function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 -- Configure LSP servers
