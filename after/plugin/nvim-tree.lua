0f37625568930373d70e8ffd40a49ed79341eed6
Gordon Wells <gcwells92@gmail.com>
2023-04-11 18:59:44 +0100

    Add missing nvim-tree config

---
+++ after/plugin/nvim-tree.lua
@@ -0,0 +1,21 @@
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+local api = require("nvim-tree.api")
+vim.keymap.set("n", "<leader>tt", function()
+  api.tree.toggle({
+    find_file = true,
+    focus = true
+  })
+end)
