434d041ac7b90b7b79667405cf2ca0de4673b51a
Gordon Wells <gcwells92@gmail.com>
2024-04-02 19:06:18 +0100

    Add default multiline arg for isort

--- neovim/.config/nvim/lua/user/lsp/null-ls.lua
+++ neovim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -13,7 +13,7 @@ null_ls.setup({
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "-l", "99" } }),
-		formatting.isort,
+		formatting.isort.with({ extra_args = { "--multi-line", "3" } }),
 		formatting.stylua,
 		diagnostics.flake8.with({ extra_args = { "--max-line-length", "99" } }),
 		formatting.shfmt,
